9da3603dc6ff17329784bd73d8e6132dba3709b7
dimawi <124430243+dimawi@users.noreply.github.com>
2023-02-11 14:34:13 +0600

    Update Main.java

--- problem-A/Main.java
+++ problem-A/Main.java
@@ -19,66 +19,6 @@ public class dima{
 
 }
 
- class Animal {
-    public String name;
 
-    public Animal(){
-
-    }
-    //constuctor
-    public Animal(String name){
-        this.name = name;
-    }
-    public String toString(){
-        return "Animal[name=" + name + "]";
-    }
-
-}
-class Mammal extends Animal{
-    public Mammal(){
-
-    }
-     public Mammal(String name){
-         super(name);
-     }
-     public String toString(){
-         return "Mammal[" +super.toString() + "]";
-     }
-}
-class Cat extends Mammal{
-    public Cat(){
-
-    }
-    public Cat(String name){
-        super(name);
-    }
-    public void greets(){
-        System.out.println("Meow");
-    }
-
-    @Override
-    public String toString() {
-        return "Cat[" + super.toString() + "]";
-    }
-}
-class Dog extends Mammal{
-    public Dog(){
-
-    }
-    public Dog(String name){
-        super(name);
-
-    }
-    public void greets(){
-        System.out.println("Woof");
-    }
-    public void greets(Dog another){
-        System.out.println("Woooof");
-    }
-    public String toString(){
-        return "Dog[" + super.toString() + "]";
-    }
-
-}
 
 
